2237af813c827b0b71a1cd7d1d160f8dcfd16856
sobolv_a <lera.sobol.26@gmail.com>
2021-05-28 12:26:48 +0300

    Some changes

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -58,41 +58,41 @@
     </div>
 </div>
 <div class="container">
-    <?php
-    /*
-    * создаем новое подключение к API Instagram, о том, как получить $token, написано выше;
-    */
-    $token = 'IGQVJWd1lTR0s5NXF0SjBZAT0lTSGJMNzRyZAnlmMFYwbU1HRnRoSXpsbXhDekcyOTcwRUxRQ3ZAPVFZAsSDdWQkEtX2ZAlS1R0TjlOOUdHc3h6bWkxZAEZAEYXJfcEVBcGNvY0pGZAktuVGtlQnFrdGtGU2J5YgZDZD';
-    /*
-    * Тут указываем либо ID пользователя, либо "self" для вывода фото владельца токена
-    * Как получить ID? Да в том же инструменте, в котором вы получали токен
-    */
-    $user_id = '436047751017879';
-    $instagram_cnct = curl_init(); // инициализация cURL подключения
-    curl_setopt( $instagram_cnct, CURLOPT_URL, "https://api.instagram.com/v1/users/" . $user_id . "/media/recent?access_token=" . $token ); // подключаемся
-    curl_setopt( $instagram_cnct, CURLOPT_RETURNTRANSFER, 1 ); // просим вернуть результат
-    curl_setopt( $instagram_cnct, CURLOPT_TIMEOUT, 15 );
-    $media = json_decode( curl_exec( $instagram_cnct ) ); // получаем и декодируем данные из JSON
-    curl_close( $instagram_cnct ); // закрываем соединение
-
-    /*
-    * количество фотографий для вывода
-    */
-    $limit = 4;
-    /*
-    * размер изображений (высота и ширина одинаковые)
-    */
-    $size = 200;
-    /*
-    * функция array_slice() задает количество элементов, которые нам нужно получить из массива
-    * если нам нужно вывести все фото, тогда: foreach($media->data as $data) {
-    */
-    foreach(array_slice($media->data, 0, $limit) as $data) {
-    echo '<a href="' . $data->link . '" target="_blank">';
-        echo '<img src="'. $data->images->low_resolution->url . '" height="'.$size.'" width="'.$size.'"/>';
-        echo '</a>';
-    }
-    ?>
+<!--    --><?php
+//    /*
+//    * создаем новое подключение к API Instagram, о том, как получить $token, написано выше;
+//    */
+//    $token = 'IGQVJWd1lTR0s5NXF0SjBZAT0lTSGJMNzRyZAnlmMFYwbU1HRnRoSXpsbXhDekcyOTcwRUxRQ3ZAPVFZAsSDdWQkEtX2ZAlS1R0TjlOOUdHc3h6bWkxZAEZAEYXJfcEVBcGNvY0pGZAktuVGtlQnFrdGtGU2J5YgZDZD';
+//    /*
+//    * Тут указываем либо ID пользователя, либо "self" для вывода фото владельца токена
+//    * Как получить ID? Да в том же инструменте, в котором вы получали токен
+//    */
+//    $user_id = '436047751017879';
+//    $instagram_cnct = curl_init(); // инициализация cURL подключения
+//    curl_setopt( $instagram_cnct, CURLOPT_URL, "https://api.instagram.com/v1/users/" . $user_id . "/media/recent?access_token=" . $token ); // подключаемся
+//    curl_setopt( $instagram_cnct, CURLOPT_RETURNTRANSFER, 1 ); // просим вернуть результат
+//    curl_setopt( $instagram_cnct, CURLOPT_TIMEOUT, 15 );
+//    $media = json_decode( curl_exec( $instagram_cnct ) ); // получаем и декодируем данные из JSON
+//    curl_close( $instagram_cnct ); // закрываем соединение
+//
+//    /*
+//    * количество фотографий для вывода
+//    */
+//    $limit = 4;
+//    /*
+//    * размер изображений (высота и ширина одинаковые)
+//    */
+//    $size = 200;
+//    /*
+//    * функция array_slice() задает количество элементов, которые нам нужно получить из массива
+//    * если нам нужно вывести все фото, тогда: foreach($media->data as $data) {
+//    */
+//    foreach(array_slice($media->data, 0, $limit) as $data) {
+//    echo '<a href="' . $data->link . '" target="_blank">';
+//        echo '<img src="'. $data->images->low_resolution->url . '" height="'.$size.'" width="'.$size.'"/>';
+//        echo '</a>';
+//    }
+//    ?>
 </div>
 <div class="footer">
     <div class="footer_cont">
